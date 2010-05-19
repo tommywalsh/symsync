a20975fe58bc486a56cd974aeb2ede1f7d1fae87
Tom Walsh <twalsh@${HOSTNAME}.(none)>
2010-05-19 08:54:04 -0400

    Don't hardcode paths.  Use command-line arguments instead.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <QApplication>
 #include <QDir>
+#include <iostream>
 #include "SyncTreeWidget.hpp"
+
+
 
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    SyncTreeWidget tw(QDir("/media/mcotp/mcotp"), QDir("/media/mcotp/mobile"));
-    tw.show();
+    bool success = false;
+    if (argc == 3) {
+        QDir srcDir = QDir(argv[1]);
+        QDir dstDir = QDir(argv[2]);
 
-    app.exec();	   
+        if (srcDir.exists()) {
+            if(dstDir.exists()) {
+                std::cout << "loading...";
+                std::cout.flush();
+                SyncTreeWidget tw(srcDir, dstDir);
+                std::cout << "done.\n";
+                tw.show();
+                
+                app.exec();
+                success = true;
+            }
+        }
+    }
+
+    if (!success) {
+        std::cout << "Usage: " << argv[0] << " srcdir dstdir\n";
+        std::cout << "    Both srcdir and dstdir must exist\n";
+        std::cout << "\n";
+        std::cout << "Makes/destroys symlinks in dstdir that point to srcdir\n";
+    }
+        
+
 }
